5c0be8c235d4a2c588ed62639d06fdb67f100386
g150446 <g150446@gmail.com>
2021-10-17 16:57:10 +0900

    Update ver.py

--- src/ver.py
+++ src/ver.py
@@ -1,11 +1,11 @@
 import datetime
 
 dt_now = datetime.datetime.now()
-
+dtstr = dt_now.strftime('%Y-%m-%d %H:%M:%S')
 with open("original.html") as f:
 
     s=f.read()
 
 with open("index.html","w") as f:
 
-  f.write(s.replace(".js", f".js?ver={dt_now.microsecond}"))
+  f.write(s.replace(".js", f".js?ver={dt_now.microsecond}").replace("Running", dtstr) )
